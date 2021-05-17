45acf03ad44869055866e889d820d9a3de5a0434
Bilal Malik <bilalmalik4321@gmail.com>
2021-05-17 17:01:51 -0400

    added stickey nav and project information

--- src/components/pages/homePage/homeProjects/homeProjects.tsx
+++ src/components/pages/homePage/homeProjects/homeProjects.tsx
@@ -5,9 +5,38 @@
 * Description: This component renders the Home page, seen when entering the app on route '/'
  */
 
+import { ProjectIntro } from "./projectIntro";
+
 export const HeaderProjects: React.FC  = () => {
+    const projectContents = [
+        {
+            title: "Safe@Work",
+            organization:"WSIB Innovation Lab",
+            description:"Protecting WSIB employees from COVID-19, as they return to work. "
+        },
+        {
+            title: "Safe@Lab",
+            organization:"WSIB Innovation Lab",
+            description:"Protecting WSIB employees from COVID-19, as they return to work. "
+        },{
+            title: "Passport",
+            organization:"WSIB Innovation Lab",
+            description:"Protecting WSIB employees from COVID-19, as they return to work. "
+        },{
+            title: "WinHacks",
+            organization:"WinHacks Organization",
+            description:"Protecting WSIB employees from COVID-19, as they return to work. "
+        }
+    ]
     return (
-        <>Header Projects</>
+        <div className="project-home-container">
+            {   projectContents.map((project)=>(
+                <div style={{paddingBottom: '2vh'}}>
+                <ProjectIntro title={project.title} organization={project.organization} description={project.description}/>
+                </div>
+            ))
+            } 
+        </div>
     );
 }
 
--- src/components/pages/homePage/homeProjects/projectIntro.tsx
+++ src/components/pages/homePage/homeProjects/projectIntro.tsx
@@ -4,9 +4,22 @@
 * Last Modified on: May, 10th, 2021
 * Description: This component renders the Home page, seen when entering the app on route '/'
  */
+interface ProjectIntroProps {
+    title: String,
+    organization: String,
+    description: String,
+}
 
-export const HeaderProjects: React.FC  = () => {
+export const ProjectIntro: React.FC<ProjectIntroProps>  = (props) => {
+    
     return (
-        <>Project Intro</>
+        <div className="project-home-box">
+            <div style={{display: "flex",flexDirection:"column"}}>
+                <h1>{props.title}</h1>
+                <h3>{props.organization}</h3>
+                <h6>{props.description}</h6>
+                <button className="check-out-project">explore</button>
+            </div>
+        </div>
     );
 }
