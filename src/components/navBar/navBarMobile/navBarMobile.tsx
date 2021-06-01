dcabf3b6c68af0f1df5a52303f1638385dc78bd6
Bilal Malik <bilalmalik4321@gmail.com>
2021-05-31 23:23:51 -0400

    finished mobile nav functionality and display

--- src/components/navBar/navBarMobile/navBarMobile.tsx
+++ src/components/navBar/navBarMobile/navBarMobile.tsx
@@ -10,6 +10,7 @@ import { ReactComponent as Close } from "images/close.svg";
 import { useNavBarContext } from "../navBarContext";
 import { NavBarModal } from "./navBarModal";
 import "styles/navBarMobile.css";
+import { Link } from "react-router-dom";
 
 export const NavBarMobile: React.FC = () => {
     
@@ -17,20 +18,22 @@ export const NavBarMobile: React.FC = () => {
     
     return(
         <ul className="nav-bar-container">
-            <li className="mobile-nav-link">
-                <h2>Bilal Malik</h2>
+            <li className="mobile-nav-link-container">
+                <Link className="nav-link" to="/" onClick={()=>setHamburgerOpen(false)}>
+                    <h2 className="mobile-nav-link">Bilal Malik</h2>
+                </Link>
             </li>
             <li>
                 { hamburgerOpen ? (
-                        <Close className="close-padding" onClick={()=>setHamburgerOpen(false)}/>
+                        <Close className="close-padding" onClick={()=>{setHamburgerOpen(false);console.log("pressed")}}/>
                     ):(
-                        <Hamburger className="hamburger-padding" onClick={()=>setHamburgerOpen(true)}/>
+                        <Hamburger className="hamburger-padding" onClick={()=>{setHamburgerOpen(true);console.log("pressed")}}/>
                     )
                 }
             </li>
             { hamburgerOpen && 
-            <div className="modal-component-class">
-                <NavBarModal/>
+            <div className={`modal-component-class`}>
+                <NavBarModal closeHamburger={()=>setHamburgerOpen(false)}/>
             </div>
             }
         </ul>
--- src/components/navBar/navBarMobile/navBarModal.tsx
+++ src/components/navBar/navBarMobile/navBarModal.tsx
@@ -1,7 +1,23 @@
-export const NavBarModal = () => {
-    return(
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface PropType {
+    closeHamburger:Function,
+}
+
+export const NavBarModal = ({closeHamburger}:PropType) => (
         <div className="modal-container">
-        Welcome to the Nav
+            <Link className="nav-link" to="experience" onClick={()=>closeHamburger()}>
+                <h2 className="mobile-nav-link">Experience</h2>
+            </Link>
+            <Link className="nav-link" to="about-me" onClick={()=>closeHamburger()}>
+                <h2 className="mobile-nav-link">About Me</h2>
+            </Link>
+            <Link className="nav-link" to="about-me" onClick={()=>closeHamburger()}>
+                <h2 className="mobile-nav-link">Projects</h2>
+            </Link>
+            <Link className="nav-link" to="about-me" onClick={()=>closeHamburger()}>
+                <h2 className="mobile-nav-link">Resume</h2>
+            </Link>
         </div>
     )
-}
